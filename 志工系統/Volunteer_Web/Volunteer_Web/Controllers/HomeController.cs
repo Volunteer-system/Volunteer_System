c90932e8029862656412ea7a479ffa13e98a4d31
peter790429 <41939921+peter790429@users.noreply.github.com>
2018-11-27 13:58:56 +0800

    2018/11/27

--- 志工系統/Volunteer_Web/Volunteer_Web/Controllers/HomeController.cs
+++ 志工系統/Volunteer_Web/Volunteer_Web/Controllers/HomeController.cs
@@ -20,9 +20,51 @@ namespace Volunteer_Web.Controllers
             return View();
         }
         //新志工申請
-        public ActionResult NewVolunteer()
+        public ActionResult NewVolunteer(int id=1)
         {
+            switch (id)
+            {
+                case 1:
+                    ViewBag.Partilview = "Requirements";
+                    break;
+                case 2:
+                    ViewBag.Partilview = "basic_info";
+                    break;
+                case 3:
+                    ViewBag.Partilview = "Questionnaire";
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    ViewBag.Partilview = "Requirement_ok";
+                    break;
+            }
+            
             return View();
+        }
+
+        public ActionResult Requirements()
+        {
+            ViewBag.Partilview = "Requirements";
+            return PartialView();
+        }
+
+        public ActionResult basic_info()
+        {
+            ViewBag.Partilview = "basic_info";
+            return PartialView();
+        }
+
+        public ActionResult Questionnaire()
+        {
+            ViewBag.Partilview = "Questionnaire";
+            return PartialView();
+        }
+
+        public ActionResult Requirement_ok()
+        {
+            ViewBag.Partilview = "Requirement_ok";
+            return PartialView();
         }
     }
 }
