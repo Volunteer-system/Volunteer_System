21ea0df71c0ff5ef6d148e2ce37d7ea47b8ef669
peter790429 <41939921+peter790429@users.noreply.github.com>
2018-11-28 21:59:18 +0800

    2018/11/28 p.m.2

--- 志工系統/Volunteer_Web/Volunteer_Web/Controllers/VolunteerController.cs
+++ 志工系統/Volunteer_Web/Volunteer_Web/Controllers/VolunteerController.cs
@@ -3,11 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Volunteer_Web.Models;
 
 namespace Volunteer_Web.Controllers
 {
-    public class VolunteerController : Controller
-    {
+    public class VolunteerController : NeedCheckLogin////Controller
+    {   //繼承登入帳戶驗證
+        public VolunteerController()
+        {   //設定要執行驗證
+            IsCheck = true;
+        }
         // GET: Volunteer
         //志工基本資料
         public ActionResult Index()
@@ -20,9 +25,35 @@ namespace Volunteer_Web.Controllers
             return View();
         }
         //排班意願
+        [HttpGet]
         public ActionResult Schedule()
         {
+            ViewBag.UserID = Session["UserID"];
             return View();
+        }
+        [HttpPost]
+        public ActionResult Schedule(int id,int[] wishid,List<int[]> service_no)
+        {
+            if (id != 0)
+            {
+                Repository<Service_period2> db = new Repository<Service_period2>();
+                db.Insert_Volunteer_Service_period(id, wishid, service_no);
+                return Content("新增/修改成功","text/plain");
+            }
+            return View();
+        }
+        //讀取已排過班的資料
+        public JsonResult GetJson()
+        {  
+            VolunteerEntities db = new VolunteerEntities();
+            int userID = Convert.ToInt32(Session["UserID"]);
+
+            var volunteer_user = db.Service_period2.Where(v => v.Volunteer_no == userID).Select(v => new
+            {
+                v.Wish_order, v.Service_period_no
+            });
+            return Json(volunteer_user, JsonRequestBehavior.AllowGet);
+            
         }
     }
 }
--- 志工系統/Volunteer_Web/Volunteer_Web/Models/Repository.cs
+++ 志工系統/Volunteer_Web/Volunteer_Web/Models/Repository.cs
@@ -44,8 +44,28 @@ namespace Volunteer_Web.Models
             dbContext.Entry(_entity).State = EntityState.Modified;
             dbContext.SaveChanges();
         }
-
-
+        //========================================三益預排表用▼=======================================================
+        public void Insert_Volunteer_Service_period(int Volunteer_no, int[] Wish_order, List<int[]> Service_period_no)
+        {
+           var user_detail = dbContext.Service_period2.Where(v => v.Volunteer_no == Volunteer_no).ToList();
+            foreach (var del in user_detail)
+            {
+                dbContext.Service_period2.Remove(del);
+            }
+            for (int i = 0; i < Wish_order.Length; i += 1)
+            {
+                for (int j = 0; j < Service_period_no[i].Length; j += 1)
+                {
+                    Service_period2 sp = new Service_period2();
+                    sp.Volunteer_no = Volunteer_no;
+                    sp.Wish_order = Wish_order[i];
+                    sp.Service_period_no = Service_period_no[i][j];
+                    dbContext.Service_period2.Add(sp);
+                }
+            }
+            dbContext.SaveChanges();
+        }
+        //========================================三益預排表用▲=======================================================
     }
-        
+
 }
